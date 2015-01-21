ae9cb5fe5efd4bcca531840efd9f98c8fc863612
Tusko Trush <tusko@photoinside.me>
2015-01-21 21:01:24 +0200

    updates for Google Map shortcode. Now you can use with StreetView mode

--- shortcodes.php
+++ shortcodes.php
@@ -228,8 +228,11 @@ function custom_register_new_user() {
     exit();
 }
 
+
 /* google map shortcode
-    *** Useing [googlemap id="somemapid" coordinates="1 ,1" zoom="17" height="500px" infobox="<p>Some Infobox Content</p>"]
+    *** Using [googlemap id="somemapid" coordinates="1 ,1" zoom="17" height="500px" infobox="<p>Some Infobox Content</p>"]
+    *** if need street view, please add 'streetview="true"';
+    *** if you need satellite view in 45 angle add 'tilt="45"';
 */
 function google_map_js($atts) {
     extract(shortcode_atts(array(
@@ -242,10 +245,14 @@ function google_map_js($atts) {
         'scaleControl'      => 'false',
         'disableDefaultUI'  => 'false',
         'infobox'           => '',
-        'tilt'              => ''
+        'tilt'              => '',
+        'icon'              => theme().'/images/marker.png',
+        'streetview'        => ''
     ), $atts));
     $mapid = str_replace('-','_',$id);
-    $map = '<div class="googlemap" id="'.$id.'" '.($height?'style="height:'.$height.'"':'').'></div><script>var '.$mapid.';
+    $map = '';
+    $map .= '<div class="googlemap" id="'.$id.'" '.($height?'style="height:'.$height.'"':'').'></div><script>
+var '.$mapid.';
 function initialize_'.$mapid.'() {
     var myLatlng = new google.maps.LatLng('.$coordinates.');
     var mapOptions = {
@@ -257,16 +264,17 @@ function initialize_'.$mapid.'() {
         scaleControl: '.$scaleControl.',
         disableDefaultUI: '.$disableDefaultUI.'
     };
-var '.$mapid.' = new google.maps.Map(document.getElementById("'.$id.'"), mapOptions);
-'.($tilt?$mapid.'.setTilt(45);':'').'
-var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: '.$mapid.',
-    animation: google.maps.Animation.DROP
-});
-'.($infobox?'marker.info = new google.maps.InfoWindow({content: "'.$infobox.'"});google.maps.event.addListener(marker, "click", function() {marker.info.open('.$mapid.', marker);});':'').'
+    var '.$mapid.' = new google.maps.Map(document.getElementById("'.$id.'"), mapOptions);
+    '.($tilt?$mapid.'.setTilt(45);':'').'
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: '.$mapid.',
+        '.($icon?'icon:"'.$icon.'",':'').'
+        animation: google.maps.Animation.DROP
+    });
+    '.($infobox?'marker.info = new google.maps.InfoWindow({content: "'.$infobox.'"});google.maps.event.addListener(marker, "click", function() {marker.info.open('.$mapid.', marker);});':'').'
 
-google.maps.event.addListener('.$mapid.', "center_changed", function() {
+    google.maps.event.addListener('.$mapid.', "center_changed", function() {
         window.setTimeout(function() {
             '.$mapid.'.panTo(marker.getPosition());
         }, 15000);
@@ -274,9 +282,32 @@ google.maps.event.addListener('.$mapid.', "center_changed", function() {
 };
 google.maps.event.addDomListener(window, "load", initialize_'.$mapid.');
 </script>';
+    $map .= $streetview?do_streetView_map($id, $coordinates, $height):'';
     return $map;
 }
 add_shortcode('googlemap', 'google_map_js');
+
+function do_streetView_map($id, $pos, $height){
+    return '<div class="googlemap" id="street_'.$id.'" '.($height?'style="height:'.$height.'"':'').'></div><script>
+    function street_init_'.$id.'() {
+        var fenway = new google.maps.LatLng('.$pos.');
+        var mapOptions = {
+            center: fenway,
+            zoom: 14
+        };
+        var street_'.$id.' = new google.maps.Map(document.getElementById("street_'.$id.'"), mapOptions);
+        var panoramaOptions = {
+            position: fenway,
+            pov: {
+                heading: 1,
+                pitch: 1
+            }
+        };
+        var panorama = new google.maps.StreetViewPanorama(document.getElementById("street_'.$id.'"), panoramaOptions);
+        street_'.$id.'.setStreetView(panorama);
+    }
+    google.maps.event.addDomListener(window, "load", street_init_'.$id.');</script>';
+}
 
 function content_btn($atts,$content){
     extract(shortcode_atts(array(
