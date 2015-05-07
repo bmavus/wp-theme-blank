1aa5ff73b317358b572c2e31d5b1f538dda11977
Tusko Trush <tusko@photoinside.me>
2015-05-07 15:10:45 +0300

    remove emoji

--- functions.php
+++ functions.php
@@ -244,6 +244,11 @@ function tt_add_jscss() {
 }
 add_action('wp_enqueue_scripts', 'tt_add_jscss');
 
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 function wp_IEhtml5_js () {
     global $is_IE;
     if ($is_IE)
